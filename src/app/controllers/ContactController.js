8431fea9653b451f3b3dcea8c3d8ace733936f70
João Alves <jvsimoesalves@gmail.com>
2023-06-02 23:23:48 -0300

    Adicionei uma rota com método POST

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -18,8 +18,21 @@ class ContactController {
     res.json(contact)
   }
 
-  store() {
-    // Criar um registro
+  async store(req, res) {
+    const { name, email, phone, category_id } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const ContactExists = await ContactsRepository.findByEmail(email);
+
+    if (ContactExists) {
+      return res.status(400).json({ error: "This e-mail is already been taken" });
+    }
+
+    const contact = await ContactsRepository.create({name, email, phone, category_id});
+    res.json(contact);
   }
 
   update() {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const routes = require("./routes")
 
 const app = express();
 
+app.use(express.json())
 app.use(routes);
 
 app.listen(1807, () => console.log("Server started at http://localhost:1807"));
